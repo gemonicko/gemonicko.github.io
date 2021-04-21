f4716d325926faf2732a543a92a3bcbababa0068
Alex Shadrin <alexshd@gmail.com>
2021-04-21 14:27:20 +0300

    deployment

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a870f03d827ccb8249a073b205d6d5c2.js"
+  "/precache-manifest.c455b9de216c0407123bc9f796ee5c61.js"
 );
 
 workbox.core.skipWaiting();
